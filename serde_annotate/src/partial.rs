0fe390ba674ac7cde0f2a5c69f28ea82e3450c0d
Gary Guo <gary.guo@lowrisc.org>
2025-08-07 16:21:14 +0100

    Use non-static TypeId to replace min_specialization for `is_serde_annotate`

--- serde_annotate/src/partial.rs
+++ serde_annotate/src/partial.rs
@@ -1,30 +1,20 @@
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
-use crate::annotate::{IsDeserializer, IsSerializer};
-use crate::Deserializer as AnnotatedDeserializer;
-use crate::{Document, Error};
+use crate::Document;
+use crate::{AnnotatedSerializer, Deserializer as AnnotatedDeserializer};
 
 impl Serialize for Document {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        if serializer.is_serde_annotate() {
-            // If `serializer` is the correct type, then we can clone the
-            // Document node and return it.
-            let r: Result<Document, Error> = Ok(self.clone());
-            let result = unsafe {
-                // We have to transmute because the we can't determine at compile
-                // time that `Result<Document, Error>` is the same type as
-                // `Result<S::Ok, S::Error>`.  If the serializer is
-                // `AnnotatedSerializer`, then it must be the same.
-                std::mem::transmute_copy(&r)
-            };
-            std::mem::forget(r);
-            result
-        } else {
-            Err(serde::ser::Error::custom("Serializing document nodes is only supported with serde_annotate::AnnotatedSerializer"))
-        }
+        AnnotatedSerializer::try_specialize(
+            serializer,
+            |_| Ok(self.clone()),
+            |_| {
+                Err(serde::ser::Error::custom("Serializing document nodes is only supported with serde_annotate::AnnotatedSerializer"))
+            },
+        )
     }
 }
 
@@ -33,18 +23,14 @@ impl<'de> Deserialize<'de> for Document {
     where
         D: Deserializer<'de>,
     {
-        if deserializer.is_serde_annotate() {
-            unsafe {
-                // If the deserializer is ours, then we can simply clone the
-                // deserializer's document node.
-                let dsz: &AnnotatedDeserializer = std::mem::transmute_copy(&deserializer);
-                std::mem::forget(deserializer);
-                Ok(dsz.doc.clone())
-            }
-        } else {
-            Err(serde::de::Error::custom(
+        AnnotatedDeserializer::try_specialize(
+            deserializer,
+            |deserializer| Ok(deserializer.doc.clone()),
+            |_| {
+                Err(serde::de::Error::custom(
                 "Deserializing document nodes is only supported with serde_annotate::Deserializer",
-            ))
-        }
+                ))
+            },
+        )
     }
 }
